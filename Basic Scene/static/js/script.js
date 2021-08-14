db3faa96cfb38ed6d1f45d53a1f9e5c38ba68ac4
Manojlovic1998 <manojlovic911@hotmail.com>
2021-08-15 01:48:04 +0200

    Create Scene and render a box

---
+++ Basic Scene/static/js/script.js
@@ -0,0 +1,43 @@
+// Sizes
+const sizes = {
+    "width": 800,
+    "height": 600
+};
+
+let canvas = document.getElementById("webgl");
+
+// Scene
+const Scene = new THREE.Scene();
+
+// Geometry (1, 1, 1) units
+const Geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// Material color 0xff0000
+const Material = new THREE.MeshBasicMaterial({"color": 0xff0000});
+
+// Mesh
+const Mesh = new THREE.Mesh(Geometry, Material);
+
+// Add Mesh to the Scene using add() method
+Scene.add(Mesh);
+
+// Camera (field of view z axis, the aspect ratio)
+const PerspectiveCamera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// Position the Camera by changing value of z, y, x property
+PerspectiveCamera.position.z = 3;
+PerspectiveCamera.position.x = 1;
+PerspectiveCamera.position.y = 1;
+
+// Add Camera to the Scene using add() method
+Scene.add(PerspectiveCamera);
+
+// Renderer
+const Renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+
+// Set size for the renderer
+Renderer.setSize(sizes.width, sizes.height);
+
+// Render the scene
+Renderer.render(Scene, PerspectiveCamera);
