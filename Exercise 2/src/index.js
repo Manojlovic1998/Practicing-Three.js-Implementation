06cef36e4f4ad9c17d67f4ce112e37f0e330732e
Manojlovic1998 <manojlovic911@hotmail.com>
2021-08-23 01:29:31 +0200

    Move 3d Object using the position property

--- Exercise 2/src/index.js
+++ Exercise 2/src/index.js
@@ -21,6 +21,8 @@ const Material = new THREE.MeshBasicMaterial({"color": 0xff0000});
 // Mesh
 const Mesh = new THREE.Mesh(Geometry, Material);
 
+// Using Mesh inherited property of Object3D called position to move the object
+Mesh.position.x = 2;
 // Add Mesh to the Scene using add() method
 Scene.add(Mesh);
 
@@ -28,8 +30,6 @@ Scene.add(Mesh);
 const PerspectiveCamera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 // Position the Camera by changing value of z, y, x property
 PerspectiveCamera.position.z = 3;
-PerspectiveCamera.position.x = 1;
-PerspectiveCamera.position.y = 1;
 
 // Add Camera to the Scene using add() method
 Scene.add(PerspectiveCamera);
